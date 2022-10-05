d6d3398150ad6fed393e6c54d12fb75be6f18469
Nichita Calmic <nichita@askphill.com>
2022-10-05 19:25:35 +0300

    feat: update hero section

--- scripts/components/core/hero-slider.js
+++ scripts/components/core/hero-slider.js
@@ -2,15 +2,17 @@ import waitToLoad from '../../lib/waitToLoad';
 import choozy from '../../lib/choozy';
 
 export default window.component(async node => {
-  const { slider, prev, next } = choozy(node, null);
+  const { slider, slide, prev, next } = choozy(node, null);
 
-  await waitToLoad('Swiper');
-  // eslint-disable-next-line no-undef
-  const swiper = new Swiper(slider, {
-    slidesPerView: 1,
-    loop: true,
-  });
+  if (slide.length > 1) {
+    await waitToLoad('Swiper');
+    // eslint-disable-next-line no-undef
+    const swiper = new Swiper(slider, {
+      slidesPerView: 1,
+      loop: true,
+    });
 
-  prev.addEventListener('click', () => swiper.slidePrev());
-  next.addEventListener('click', () => swiper.slideNext());
+    prev.addEventListener('click', () => swiper.slidePrev());
+    next.addEventListener('click', () => swiper.slideNext());
+  }
 });

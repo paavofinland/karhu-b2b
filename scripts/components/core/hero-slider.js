4371d3c6d3b3a54e3bee044e80132d61c9dc0e0a
Wiebe Kaai <wiebekaai@gmail.com>
2022-10-17 13:50:05 +0200

    refactor(section.hero): no jump, intellisense types and remove global styles

--- scripts/components/core/hero-slider.js
+++ scripts/components/core/hero-slider.js
@@ -6,11 +6,20 @@ export default window.component(async node => {
 
   if (slide.length > 1) {
     await waitToLoad('Swiper');
-    // eslint-disable-next-line no-undef
-    const swiper = new Swiper(slider, {
+
+    /** @type {import('swiper').SwiperOptions} */
+    const swiperOptions = {
       slidesPerView: 1,
       loop: true,
-    });
+      on: {
+        afterInit: s => {
+          s.wrapperEl.classList.remove('overflow-hidden', 'flex');
+        },
+      },
+    };
+
+    // eslint-disable-next-line no-undef
+    const swiper = new Swiper(slider, swiperOptions);
 
     prev.addEventListener('click', () => swiper.slidePrev());
     next.addEventListener('click', () => swiper.slideNext());

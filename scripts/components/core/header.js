8611eb82e19b0a9e295dd6d56e914ebc7f117caa
Wiebe Kaai <wiebekaai@gmail.com>
2022-10-18 18:28:30 +0200

    feat(header): tailwind transitions

--- scripts/components/core/header.js
+++ scripts/components/core/header.js
@@ -1,48 +1,34 @@
-/* eslint-disable consistent-return */
 import choozy from '../../lib/choozy';
-
-const SCREEN_MD = 768;
 
 export default window.component(node => {
   const { hamburger, hamburgerOpened, hamburgerClosed, mobileMenu } = choozy(node, null);
 
-  const announcementBarHeight = document.querySelector('#announcement-bar')?.offsetHeight || 0;
-
-  const setHeaderWhiteTheme = () => {
-    node.classList.add('header_scrolled', 'border-grey-5');
-    node.classList.remove('border-opacity-20');
-  };
-
-  const setHeaderBasicTheme = () => {
-    node.classList.remove('header_scrolled', 'border-grey-5');
-    node.classList.add('border-opacity-20');
-  };
+  const announcementBarHeight =
+    document.querySelector('#shopify-section-announcement-bar').offsetHeight || 0;
 
   const isMenuClosed = () => hamburgerOpened.classList.contains('hidden');
   const isHeaderScrolled = () => window.scrollY > announcementBarHeight;
 
-  const handleScrollHeader = () =>
-    isMenuClosed() && (isHeaderScrolled() ? setHeaderWhiteTheme() : setHeaderBasicTheme());
+  const toggleTheme = () =>
+    node.classList[!isMenuClosed() || isHeaderScrolled() ? 'add' : 'remove']('is-active');
 
-  const handleClickHamburgerMenu = () =>
-    !isHeaderScrolled() && (isMenuClosed() ? setHeaderBasicTheme() : setHeaderWhiteTheme());
-
-  window.addEventListener('scroll', handleScrollHeader);
-  window.addEventListener('load', handleScrollHeader);
-  node.addEventListener('mouseover', setHeaderWhiteTheme);
-  node.addEventListener('mouseleave', () => !isHeaderScrolled() && setHeaderBasicTheme());
+  window.addEventListener('scroll', toggleTheme, { passive: true });
+  window.addEventListener('load', toggleTheme);
 
   hamburger.addEventListener('click', () => {
     hamburgerOpened.classList.toggle('hidden');
     hamburgerClosed.classList.toggle('hidden');
     document.body.classList.toggle('overflow-hidden');
     mobileMenu.classList.toggle('left-0');
-    handleClickHamburgerMenu();
+    toggleTheme();
   });
 
-  if (window.innerWidth <= SCREEN_MD) {
+  const setMobileMenuHeight = () => {
     mobileMenu.style.height = `${
       window.innerHeight - announcementBarHeight - mobileMenu.parentElement.offsetHeight
     }px`;
-  }
+  };
+
+  setMobileMenuHeight();
+  window.addEventListener('resize', setMobileMenuHeight);
 });

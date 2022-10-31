d4397e6de6026757b567e953474f92e6947c1177
Wiebe Kaai <wiebekaai@gmail.com>
2022-10-31 14:10:10 +0100

    feat(product): switch sizes locale

--- scripts/components/product/components.js
+++ scripts/components/product/components.js
@@ -1,9 +1,11 @@
 import productForm from './product-form';
 import productImages from './product-images';
 import productColors from './product-colors';
+import productSizes from './product-sizes';
 
 export default {
   productForm,
   productImages,
   productColors,
+  productSizes,
 };

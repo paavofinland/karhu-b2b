0776bc4f196d1568d6be22c68e1007740dee0d61
Nichita Calmic <nichita@askphill.com>
2022-11-01 17:35:27 +0200

    feat: create cart page layout

--- scripts/components/sections/cart.js
+++ scripts/components/sections/cart.js
@@ -1,20 +1,31 @@
 import choozy from '../../lib/choozy';
 
 export default window.component(async node => {
-  const { editItemBtn, sidebar, sidebarLayer, closeSidebarBtn } = choozy(node, null);
+  const { editItemBtn, sidebar, sidebarLayer, closeSidebarBtn, saveCartBtn, popup, closePopupBtn } =
+    choozy(node, null);
 
+  console.log(closePopupBtn);
   const getElementByDataId = id => element => element.dataset.id === id;
 
   const sidebarList = Array.isArray(sidebar) ? Array.from(sidebar) : [sidebar];
 
   const onToggleSidebar = e => {
-    const { id: itemId } = e.target.dataset;
+    const { id: itemId } = e.currentTarget.dataset;
     const sidebarEl = sidebarList.find(getElementByDataId(itemId));
     sidebarEl.classList.toggle('hidden');
+    sidebarEl.classList.toggle('flex');
     sidebarLayer.classList.toggle('hidden');
+    sidebarLayer.classList.toggle('flex');
     document.body.classList.toggle('overflow-hidden');
   };
 
   editItemBtn.addEventListener('click', onToggleSidebar);
   closeSidebarBtn.addEventListener('click', onToggleSidebar);
+
+  const onToggleSaveCartPopup = () => {
+    popup.classList.toggle('hidden');
+  };
+
+  saveCartBtn.addEventListener('click', onToggleSaveCartPopup);
+  closePopupBtn.forEach(btn => btn.addEventListener('click', onToggleSaveCartPopup));
 });

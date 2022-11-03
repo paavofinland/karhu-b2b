c09e5424282cf3711715a9d84031277ff9950771
Nichita Calmic <nichita@askphill.com>
2022-11-03 15:39:31 +0200

    feat: add to cart multiple variants

--- scripts/components/product/product-form.js
+++ scripts/components/product/product-form.js
@@ -1,42 +1,25 @@
-import { addVariant } from '../../lib/cart';
 import choozy from '../../lib/choozy';
-import { getString } from '../../lib/string';
 
 export default window.component((node, ctx) => {
-  const { submit } = choozy(node, null);
+  const { formCartAdd, addToCartButton } = choozy(node, null);
 
-  let currentVariant = {};
-
-  ctx.on('variant:change', (state, variant) => {
-    currentVariant = variant;
-    submit.textContent = getString(variant.available ? 'product.add_to_cart' : 'product.sold_out');
-    submit.disabled = !variant.available;
-  });
-
-  node.addEventListener('submit', e => {
+  formCartAdd.addEventListener('submit', e => {
     e.preventDefault();
 
-    submit.disabled = true;
-    submit.textContent = getString('product.adding_to_cart');
+    const formData = new FormData(formCartAdd);
+    fetch(`${window.Shopify.routes.root}cart/add.js`, {
+      method: 'POST',
+      body: formData,
+    })
+      .then(response => response.json())
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  });
 
-    addVariant(currentVariant, node.elements.quantity.value).then(({ error }) => {
-      // eslint-disable-next-line no-alert
-      if (error) alert(error);
-
-      submit.disabled = true;
-      submit.textContent = getString('product.added_to_cart');
-
-      setTimeout(() => {
-        if (!error) ctx.emit('cart:toggle', { cartOpen: true });
-      }, 0);
-
-      setTimeout(
-        () => {
-          submit.disabled = false;
-          submit.textContent = getString('product.add_to_cart');
-        },
-        error ? 0 : 1000
-      );
-    });
+  ctx.on('button:enable', () => {
+    if (addToCartButton.hasAttribute('disabled')) {
+      addToCartButton.removeAttribute('disabled');
+    }
   });
 });

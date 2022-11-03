8e2ea1de97cab6cfcb4973d086bb3b5ad2bc3811
Nichita Calmic <nichita@askphill.com>
2022-11-03 15:39:31 +0200

    feat: add to cart multiple variants

--- scripts/components/product/product-sizes.js
+++ scripts/components/product/product-sizes.js
@@ -1,9 +1,31 @@
 import choozy from '../../lib/choozy';
 
-export default window.component(node => {
-  const { size, sizeButton } = choozy(node);
+export default window.component((node, ctx) => {
+  const { size, sizeButton, quantityInputs } = choozy(node);
   const sizeLabels = [].concat(size).filter(Boolean);
   const sizeButtons = [].concat(sizeButton).filter(Boolean);
+
+  const inputList = Array.isArray(quantityInputs) ? quantityInputs : [...quantityInputs];
+
+  const onChangeQuantity = e => {
+    if (e.target.value > 0 || inputList.some(elem => elem.value > 0)) {
+      ctx.emit('button:enable', null);
+    }
+  };
+
+  const validateNumberInput = e => {
+    if (e.target.value === '0') {
+      e.target.value = e.target.value.slice(1);
+    }
+  };
+
+  inputList.forEach(input => {
+    input.addEventListener('change', onChangeQuantity);
+    input.addEventListener('keypress', e => {
+      onChangeQuantity(e);
+      validateNumberInput(e);
+    });
+  });
 
   sizeButtons.forEach(button =>
     button.addEventListener('click', e => {
@@ -18,6 +40,4 @@ export default window.component(node => {
       button.classList.add('is-active');
     })
   );
-
-  console.log(size);
 });

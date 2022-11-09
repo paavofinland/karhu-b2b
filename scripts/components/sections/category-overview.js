dffd5f8cd44647161e8c2db34fcdd6fc07409850
Wiebe Kaai <wiebekaai@gmail.com>
2022-11-09 17:06:02 +0100

    styling(category-overview): quick touch ups

--- scripts/components/sections/category-overview.js
+++ scripts/components/sections/category-overview.js
@@ -5,13 +5,37 @@ export default window.component(async node => {
   const { slider } = choozy(node, null);
 
   await waitToLoad('Swiper');
+
+  /** @type {import('swiper').SwiperOptions} */
+  const swiperOptions = {
+    slidesPerView: 1.4,
+    spaceBetween: 16,
+    slidesOffsetBefore: 16,
+    slidesOffsetAfter: 16,
+    breakpoints: {
+      768: {
+        slidesPerView: 2.2,
+        spaceBetween: 16,
+        slidesOffsetBefore: 16,
+        slidesOffsetAfter: 16,
+      },
+      1024: {
+        slidesPerView: 2.4,
+        spaceBetween: 60,
+        slidesOffsetBefore: 0,
+        slidesOffsetAfter: 0,
+      },
+      1440: {
+        slidesPerView: 3,
+        spaceBetween: 60,
+        slidesOffsetBefore: 0,
+        slidesOffsetAfter: 0,
+      },
+    },
+  };
+
   // eslint-disable-next-line no-undef
-  const swiper = new Swiper(slider, {
-    allowTouchMove: true,
-    direction: 'horizontal',
-    slidesPerView: 'auto',
-    grabCursor: true,
-  });
+  const swiper = new Swiper(slider, swiperOptions);
 
   return swiper;
 });

0bab5342ee576a8a749efde77d4a308f3cec3935
Nichita Calmic <nichita@askphill.com>
2022-11-09 14:53:25 +0200

    refactor: update search functionality code, add sort by option

--- scripts/components/sections/search.js
+++ scripts/components/sections/search.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable no-param-reassign */
 import choozy from '../../lib/choozy';
 import { debounce } from '../../lib/debounce';
@@ -10,28 +11,44 @@ const sections = {
   loadMore: '[data-load-more]',
   searchResults: '[data-search-results]',
   empty: '[data-empty]',
+  productsCount: '[data-products-count]',
+  sortByOptions: '[data-sort-by-options]',
 };
 
-const getQueryParams = ({ page, sectionId, value }) => {
+const getQueryString = ({ page, sectionId = '', q, sortBy }) => {
   const queryParams = new URLSearchParams(window.location.search);
   queryParams.set('page', page);
-  queryParams.set('q', value);
+  queryParams.set('q', q);
   queryParams.set('sectionId', sectionId);
+  queryParams.set('sort_by', sortBy);
   queryParams.set('type', 'product');
   return queryParams.toString();
 };
 
+const replaceEmptyParams = ({ page, q, sortBy }) => {
+  return {
+    page: page || getParameter('page'),
+    q: q || getParameter('q'),
+    sortBy: sortBy || getParameter('sort_by'),
+  };
+};
+
 const onSearch = sectionId =>
-  debounce(async (value, page) => {
-    const query = getQueryParams({ page, value, sectionId });
+  debounce(async queryData => {
+    const { page, q, sortBy } = replaceEmptyParams(queryData);
+    const query = getQueryString({ page, q, sectionId, sortBy });
     const productsHtml = await fetchHtml(
       `${window.location.origin + window.location.pathname}?${query}`
     );
-    window.app.emit(['search:render'], null, { html: productsHtml, uri: query });
+    window.app.emit(['search:render'], null, {
+      html: productsHtml,
+      uri: query,
+      requestedPage: page,
+    });
   });
 
 export default window.component(async (node, ctx) => {
-  const { searchInput, searchContainer } = choozy(node, null);
+  const { searchInput, searchContainer, sortByOptions } = choozy(node, null);
 
   const onSearchProducts = onSearch(node.dataset.sectionId);
 
@@ -40,37 +57,52 @@ export default window.component(async (node, ctx) => {
       entries.forEach(async entry => {
         if (entry.isIntersecting) {
           const currentPage = getParameter('page');
-          onSearchProducts(searchInput.value, +currentPage + 1);
+          onSearchProducts({ q: searchInput.value, page: +currentPage + 1 });
         }
       });
     },
     {
       rootMargin: '0px',
-      threshold: 0,
+      threshold: [0, 0.25, 0.5, 0.75, 1],
     }
   );
 
-  const updateSearchResults = html => {
+  const renderSearchData = html => {
     const currentPage = getParameter('page');
+    const { searchResults } = choozy(node, null);
+    const { searchResults: newSearchResultsContent } = choozy(html, null);
+
     if (currentPage === '1') {
-      node.querySelector(sections.productsContainer).innerHTML = html.querySelector(
-        sections.productsContainer
-      ).innerHTML;
+      searchResults.innerHTML = newSearchResultsContent.innerHTML;
       return;
     }
-    node
-      .querySelector(sections.searchResults)
-      .insertAdjacentHTML('beforeEnd', html.querySelector(sections.searchResults).innerHTML);
+    searchResults.insertAdjacentHTML('beforeEnd', newSearchResultsContent.innerHTML);
+  };
+
+  const updateProductsCount = html => {
+    const { productsCount } = choozy(node, null);
+    const { productsCount: newProductsCountContent } = choozy(html, null);
+    productsCount.innerHTML = newProductsCountContent.innerHTML;
+  };
+
+  const clearSearchResults = () => {
+    const { searchResults } = choozy(node, null);
+    searchResults.innerHTML = '';
+  };
+
+  const onStartLoadingState = () => {
+    const { searchResults, loader } = choozy(node, null);
+    loader.classList.remove('hidden');
+    searchResults.classList.add('hidden');
   };
 
   const onEndLoadingState = () => {
-    node.querySelector(sections.loader).classList.add('hidden');
-    node.querySelector(sections.productsContainer).classList.remove('hidden');
+    const { searchResults, loader } = choozy(node, null);
+    loader.classList.add('hidden');
+    searchResults.classList.remove('hidden');
   };
 
   const initializeOnFirstRender = () => {
-    onEndLoadingState();
-
     const loadMore = node.querySelector(sections.loadMore);
     if (loadMore) {
       observer.observe(loadMore);
@@ -78,29 +110,44 @@ export default window.component(async (node, ctx) => {
   };
 
   const onInputSearch = value => {
-    if (value) {
-      onSearchProducts(value, 1);
-    }
-    node.querySelector(sections.loader).classList.remove('hidden');
-    node.querySelector(sections.productsContainer).classList.add('hidden');
     searchContainer.classList[value ? 'add' : 'remove']('is-search');
-  };
 
-  searchInput.addEventListener('input', e => onInputSearch(e.target.value));
-
-  ctx.on('search:render', (_state, { html, uri }) => {
-    updateURLHash(uri);
-
-    const isEmpty = !html.querySelector(sections.searchResults).childNodes.length;
-    if (isEmpty) {
-      onEndLoadingState();
-      node.querySelector(sections.empty).classList.remove('hidden');
-      node.querySelector(sections.loadMore).remove();
+    if (value) {
+      onSearchProducts({ q: value, page: 1 });
+      onStartLoadingState();
       return;
     }
 
-    updateSearchResults(html);
-    if (getParameter('page') === '1') initializeOnFirstRender();
+    updateURLHash(getQueryString({ page: 1, q: '', sortBy: sortByOptions.value }));
+  };
+
+  const onChangeSortBy = e => {
+    const sortBy = e.target.value;
+    onSearchProducts({ q: searchInput.value, page: getParameter('page'), sortBy });
+  };
+
+  searchInput.addEventListener('input', e => onInputSearch(e.target.value));
+  sortByOptions.addEventListener('change', onChangeSortBy);
+
+  ctx.on('search:render', (_state, { html, uri, requestedPage }) => {
+    updateProductsCount(html);
+    const isEmpty = !html.querySelector(sections.searchResults).childNodes.length;
+    if (isEmpty) {
+      if (requestedPage === 1) {
+        clearSearchResults();
+        node.querySelector(sections.empty).classList.remove('hidden');
+      }
+      onEndLoadingState();
+      return;
+    }
+
+    updateURLHash(uri);
+    renderSearchData(html);
+    node.querySelector(sections.empty).classList.add('hidden');
+    if (requestedPage === 1) {
+      initializeOnFirstRender();
+      onEndLoadingState();
+    }
     window.app.mount();
   });
 

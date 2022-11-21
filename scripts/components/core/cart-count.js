b34b4d8c215fa6f845a92daf6f07fca06e3fd24b
Nichita Calmic <nichita@askphill.com>
2022-11-21 17:29:49 +0200

    wip: edit cart item functionality

--- scripts/components/core/cart-count.js
+++ scripts/components/core/cart-count.js
@@ -4,7 +4,7 @@ export default window.component((node, ctx) => {
   const { count: countElement } = choozy(node);
 
   ctx.on('cart:update', (_, { countAdd, count }) => {
-    if (count) {
+    if (!Number.isNaN(Number(count))) {
       countElement.innerHTML = count;
     } else if (countAdd) {
       countElement.innerHTML = Number(countElement.innerHTML) + countAdd;

219ee059924d97e65c641e000a43bebb2933d8d8
Wiebe Kaai <wiebekaai@gmail.com>
2022-12-07 13:46:02 +0100

    refactor: consistent naming account components

--- scripts/components/account/components.js
+++ scripts/components/account/components.js
@@ -1,13 +1,13 @@
 import login from './login';
 import account from './account';
 import address from './address';
-import orders from './orders';
-import accountCarts from './accountCarts';
+import accountOrders from './account-orders';
+import accountCartTemplates from './account-cart-templates';
 
 export default {
   login,
   account,
   address,
-  orders,
-  accountCarts,
+  accountOrders,
+  accountCartTemplates,
 };
--- scripts/index.js
+++ scripts/index.js
@@ -9,4 +9,4 @@ window.app = app;
 window.component = component;
 window.vendors = name => app.emit(`${name}:loaded`);
 
-app.on('mount', () => console.info('♻️ Remount'));
+app.on('mount', () => console.info('♻️ Mounting'));

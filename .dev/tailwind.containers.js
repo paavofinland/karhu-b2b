b647fd543f4cf40e374e1c4eb72dc0d34a047467
Wiebe Kaai <wiebekaai@gmail.com>
2023-01-06 10:24:03 +0100

    refactor: separate css for checkout

--- .dev/tailwind.containers.js
+++ .dev/tailwind.containers.js
@@ -1,21 +1,15 @@
 const plugin = require('tailwindcss/plugin');
 
 module.exports = plugin(({ addComponents }) => {
-    addComponents({
-      '.container': {
-        '@apply max-w-full xxl:max-w-screen-xxl lg:mx-auto': {},
-      },
-      '.container-content': {
-        '@apply w-full px-content mx-auto xxl:max-w-screen-xxl': {},
-      },
-      '.container-content-wide': {
-        '@apply container-content px-content-wide': {},
-      },
-      '.total-line--reduction, .section--payment-method, .order-summary__section--discount': {
-        '@apply hidden': {},
-      },
-      '.order-summary__section~.order-summary__section--discount~.order-summary__section--total-lines': {
-        '@apply border-none': {},
-      }
-    });
-  })
+  addComponents({
+    '.container': {
+      '@apply max-w-full xxl:max-w-screen-xxl lg:mx-auto': {},
+    },
+    '.container-content': {
+      '@apply w-full px-content mx-auto xxl:max-w-screen-xxl': {},
+    },
+    '.container-content-wide': {
+      '@apply container-content px-content-wide': {},
+    },
+  });
+});

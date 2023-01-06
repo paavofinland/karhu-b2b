3ab5eecd7061e965c47b0d511cdff1da3e7626c2
Wiebe Kaai <wiebekaai@gmail.com>
2023-01-06 10:45:36 +0100

    refactor: checkout

--- scripts/components/checkout/checkout.js
+++ scripts/components/checkout/checkout.js
@@ -1,42 +1,52 @@
 import getLiquidVariables from '../../lib/get-liquid-variables';
 
-const config = {
-  childList: true,
-  subtree: true,
-};
+const querySelect = (node, queries) => queries.map(q => node.querySelector(q));
 
-export default window.component((node, ctx) => {
-  const { skip_payment_discount_code: discountCode, skip_payment_discount_applied: applied } =
-    getLiquidVariables();
+export default window.component(node => {
+  const {
+    skip_payment_discount_code: discountCode,
+    skip_payment_discount_applied: discountApplied,
+  } = getLiquidVariables();
 
-  const subtotalEl = document.querySelector('[data-checkout-subtotal-price-target]');
-  const subtotal = subtotalEl.innerText;
-  const total = document.querySelector('.total-line [data-checkout-payment-due-target]');
-  total.innerText = subtotal;
+  const [totalSectionElement] = querySelect(node, ['.order-summary__sections']);
 
-  const totalSection = document.querySelector('.order-summary__sections');
+  const replaceTotalWithSubtotal = () => {
+    const [subtotalElement, totalElement] = querySelect(node, [
+      '[data-checkout-subtotal-price-target]',
+      '.total-line [data-checkout-payment-due-target]',
+    ]);
 
-  const callback = mutationList => {
+    if (totalElement.innerText !== subtotalElement.innerText)
+      // Check to prevent infinite loop in MutationObserver
+      totalElement.innerText = subtotalElement.innerText;
+  };
+
+  replaceTotalWithSubtotal();
+
+  new MutationObserver(mutationList => {
     mutationList.forEach(mutation => {
       const { type, addedNodes } = mutation;
       if (type !== 'childList' || !addedNodes[0]) return;
-      const newTotal =
+
+      const totalMutation =
         addedNodes[0].nodeType === Node.ELEMENT_NODE
           ? addedNodes[0].querySelector('[data-checkout-payment-due-target]')
           : 'checkoutPaymentDueTarget' in addedNodes[0].parentNode.dataset &&
             addedNodes[0].parentNode;
-      if (!newTotal || newTotal.innerText === subtotal) return;
-      newTotal.innerText = subtotal;
+
+      if (totalMutation) replaceTotalWithSubtotal();
     });
-  };
+  }).observe(totalSectionElement, {
+    childList: true,
+    subtree: true,
+  });
 
-  const observer = new MutationObserver(callback);
-  observer.observe(totalSection, config);
-
-  if (!applied) {
-    const discountInput = document.querySelector('[name="checkout[reduction_code]"]');
+  if (!discountApplied) {
+    const [discountInput, discountSubmit] = querySelect(node, [
+      '.order-summary__section--discount [name="checkout[reduction_code]"]',
+      '.order-summary__section--discount [name="checkout[submit]"]',
+    ]);
     discountInput.value = discountCode;
-    const discountSubmit = document.querySelector('[name="checkout[submit]"]');
     discountSubmit.click();
   }
 });

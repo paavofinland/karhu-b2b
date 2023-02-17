28ba7c0674e583e76f7bcf0d1064345acb2b1738
Nichita Calmic <nichita@askphill.com>
2023-02-17 17:35:50 +0200

    feat: limit line items to 500 on input change

--- scripts/components/core/cart-edit-drawer.js
+++ scripts/components/core/cart-edit-drawer.js
@@ -1,4 +1,6 @@
 import choozy from '../../lib/choozy';
+import getLiquidVariables from '../../lib/get-liquid-variables';
+import { MAX_LINE_ITEMS } from './product-form';
 
 const updateCart = async payload =>
   fetch(`${window.Shopify.routes.root}cart/update.js`, {
@@ -8,7 +10,12 @@ const updateCart = async payload =>
 
 export default window.component((node, ctx) => {
   const { productId } = node.dataset;
-  const { drawer, backdrop, submit, form, closeButton } = choozy(node);
+  const { drawer, backdrop, submit, form, closeButton, updateErrorMsg } = choozy(node);
+
+  const { line_items: lineItems } = getLiquidVariables();
+
+  let addedLineItems = [];
+  let removedLineItems = [];
 
   ctx.on('cart-edit-drawer:toggle', (_, id = false) => {
     [drawer, backdrop].forEach(e => e.classList[id === productId ? 'add' : 'remove']('is-active'));
@@ -25,8 +32,57 @@ export default window.component((node, ctx) => {
   form.addEventListener('submit', async e => {
     e.preventDefault();
     ctx.emit('cart:loading', null, true);
-    const response = await updateCart(new FormData(e.target));
+
+    const formData = new FormData(e.target);
+    Array.from(formData.entries()).forEach(([key, value]) => {
+      if (value) return;
+      formData.delete(key);
+    });
+
+    const response = await updateCart(formData);
+    addedLineItems = [];
+    removedLineItems = [];
     // TODO: Error handling?
     ctx.emit('cart:render');
+  });
+
+  const setError = (error = '') => {
+    updateErrorMsg.innerHTML = error;
+  };
+
+  form.addEventListener('keypress', e => {
+    if (e.target.value === '0') {
+      e.target.value = e.target.value.slice(1);
+    }
+  });
+
+  // TODO: refactor, remove duplication from product-form
+  form.addEventListener('input', async e => {
+    const { id: targetId, value: newQuantity } = e.target;
+    if (Number(newQuantity) > 0 && !addedLineItems.includes(targetId)) {
+      if (!lineItems.includes(targetId)) {
+        addedLineItems.push(targetId);
+      }
+      const foundIdx = removedLineItems.findIndex(id => id === targetId);
+      if (foundIdx !== -1) {
+        removedLineItems.splice(foundIdx, 1);
+      }
+    } else if (!Number(newQuantity) && !removedLineItems.includes(targetId)) {
+      if (lineItems.includes(targetId)) {
+        removedLineItems.push(targetId);
+      }
+      const foundIdx = addedLineItems.findIndex(id => id === targetId);
+      if (foundIdx !== -1) {
+        addedLineItems.splice(foundIdx, 1);
+      }
+    }
+    const tempLineItems = lineItems.filter(id => !removedLineItems.includes(id));
+    if (addedLineItems.length + tempLineItems.length > MAX_LINE_ITEMS) {
+      setError('You cannot add more than 500 items to your cart');
+      submit.disabled = true;
+      return;
+    }
+    setError();
+    submit.disabled = false;
   });
 });
